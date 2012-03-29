e64c46489bbdd8ee38528a8e4d3584fa4f2037f5
lindachhor <purplelife_16@hotmail.com>
2012-03-29 15:47:24 -0400

    adding stripslashes to db

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,7 @@
 // This is the best way to keep usernames and passwords out of public GitHub repos
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$dsn = getenv('DB_DSN');
+$dsn = stripslashes(getenv('DB_DSN'));
 
 //Open a connection to the databse and stores it in a variable
 //PDO allows us to connect to different types of databases, not just MySQL
